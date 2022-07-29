a0e83f3f04e447caca9a7a599859669aca7ff7a4
Atin Sahu <97465321+atin-sahu@users.noreply.github.com>
2022-07-29 17:13:24 +0530

    search by id

--- grocery/src/pages/product/Product.jsx
+++ grocery/src/pages/product/Product.jsx
@@ -1,11 +1,12 @@
-import { Box } from '@chakra-ui/react'
-import React, { useEffect } from 'react'
+import { Box, Container, Flex, Image, Stack, Text } from '@chakra-ui/react'
+import React, { useEffect, useState } from 'react'
 import axios from "axios";
 import { useParams } from 'react-router-dom';
 
 
 export const Product = () => {
-
+  const [item,setItem] = useState({});
+  // const [imgIndex,setImgIndex] = useState(0);
   const  { id }  = useParams();
   
 
@@ -16,13 +17,48 @@ export const Product = () => {
   
   const getData = async () => {
     const data = await axios.get(`http://localhost:8080/products/${id}`)
-    .then((data) => data.data)
-    console.log("single product",data);
+    .then((data) => setItem(data.data))
+    // console.log("single product",data);
+    console.log("setItem in",item);
   };
+  
+  console.log("setItem out",item);
 
   return (
     <Box mt="50px" bgColor="whitesmoke" px={10}>
-      product details
+      <Container  bgColor="white" maxW="container.2lg" py={2}>
+        <Box display="flex" flexDirection={{base:"column", md:"row"}} gap={2}>
+
+          <Box flex={{base:6,md:5}} >
+            <Box display="flex" flexDirection={{base:"column-reverse", md:"row"}} >
+              <Box flex={{base:6,md:2}} >
+                {/* <Stack direction={{base:"row", md:"column"}} border="1px solid black" > */}
+                  {
+                    
+                    item?.imgUrl?.map((im)=>(
+                       <Box key={im} border="1px solid black" >
+                        <Image src={im} />
+                      </Box>
+                    ))
+                  }
+                  
+                {/* </Stack> */}
+
+              </Box>
+
+              <Box display="flex" flex={{base:6,md:10}} border="1px solid gray" justifyContent="center" alignItems="center">
+                <Text>atin</Text>
+              </Box>
+
+            </Box>
+          </Box>
+
+          <Box flex={{base:6,md:7}} border="1px solid yellow">
+
+          </Box>
+
+        </Box>
+      </Container>
     </Box>
   )
 }
