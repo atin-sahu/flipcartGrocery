13518deb56a015476c4ea83860c5e547b10c48e3
Atin Sahu <97465321+atin-sahu@users.noreply.github.com>
2022-08-02 00:41:43 +0530

    cart page

--- grocery/src/pages/cart/Cart.jsx
+++ grocery/src/pages/cart/Cart.jsx
@@ -1,4 +1,5 @@
-import { Box, Divider, Image, Text } from '@chakra-ui/react'
+import { DeleteIcon } from '@chakra-ui/icons';
+import { Box, Button, Divider, Image, Text } from '@chakra-ui/react'
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 
@@ -19,29 +20,51 @@ export const Cart = () => {
   return (
     <Box mt="50px" bgColor="whitesmoke" px={10}>
         <Box p={2} display="flex" flexDirection={{base:"column", md:"row"}} gap={5}>
-          <Box flex={{base:"12",md:"8"}}  border="1px solid black" p={2}>
-            <Text  my={5} align="center" fontWeight="semibold">Your Items</Text>
+          <Box flex={{base:"12",md:"8"}}  p={2}>
+            <Text  my={5} align="center" fontWeight="semibold">YOUR ITEM's</Text>
             {items.map((item)=>{
               return <Box key={item.id} >
-                  <Box display="flex" p={2}  justifyContent="space-between">
-                    <Box display="flex" p={1} gap={2} border="1px solid green">
-                      <Box flex={3} border="1px solid red">
-                        <Image minW="70%" maxH="100px" maxW="70%" src={item.imgUrl[0]}></Image>
+                  <Box display="flex"  gap={2} _hover={{bgColor:"silver"}} p={2}>
+                    <Box flex={9} display="flex" gap={2} >
+                      <Box display="flex" alignItems="center" justifyContent="center" flex={3} p={2} >
+                        <Image maxH="100px" maxW="70%" src={item.imgUrl[0]}></Image>
                       </Box>
-                      <Box flex={9} border="1px solid red">
-                        right
+                      <Box p={2} flex={9}>
+                        <Text fontWeight="semibold">{item.title}</Text>
+                        <Text>{item.unit}</Text>
+                        <Box display="flex" gap={5} pt={4} alignItems="center">
+                          <Text fontWeight="semibold" fontSize="2xl">₹{item.price}</Text>
+                          <Text as="del" >₹{item.old_price}</Text>
+                          <Text color="green">{item.disscount} off</Text>
+                        </Box>
                       </Box>
                     </Box>
-                    <Box border="1px solid green">
-                      right
+                    <Box flex={3} >
+                      <Box border="1px solid black" borderRadius={5} align="center" mt={4}>
+                        <Button variant="unstyled" rightIcon={<DeleteIcon></DeleteIcon>}>Remove</Button>
+                      </Box>
                     </Box>
                   </Box>
                   <Divider orientation='horizontal' />
               </Box>
             })}
           </Box>
-          <Box p={2} flex={{base:"12",md:"4"}} border="1px solid black">
-            <Text my={5} align="center" fontWeight="semibold">Your Items</Text>
+          <Box p={2} flex={{base:"12",md:"4"}}  >
+            <Text my={5} align="center" fontWeight="semibold">PRICE DETAILS</Text>
+            <Box border="1px solid black">
+              <Box display="flex" p={2} gap={2} justifyContent="space-between">
+                <Text >MRP ({items.length} Items) </Text>
+                <Text fontWeight="semibold">₹{}</Text>
+              </Box>
+              <Box display="flex" b p={2} gap={2} justifyContent="space-between">
+                <Text >Product Discount</Text>
+                <Text fontWeight="semibold" color="green">₹{}</Text>
+              </Box>
+              <Box display="flex" p={2} gap={2} justifyContent="space-between">
+                <Text >Delivery Fee</Text>
+                <Text fontWeight="semibold" color="red">₹ 49</Text>
+              </Box>
+            </Box>
           </Box>
         </Box>
     </Box>
