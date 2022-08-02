0f72f2a66261967fd95c8ea39a2c5d30649ecb4b
Atin Sahu <97465321+atin-sahu@users.noreply.github.com>
2022-08-02 11:06:30 +0530

    cart page

--- grocery/src/pages/cart/Cart.jsx
+++ grocery/src/pages/cart/Cart.jsx
@@ -6,12 +6,28 @@ import React, { useEffect, useState } from 'react'
 export const Cart = () => {
 
   const [items,setItems] = useState([]);
+  const [MRP, setMRP] = useState(0);
+  const [off, setOff] = useState(0);
 
   const getCartData = async()=>{
     let data = await axios.get("http://localhost:8080/cart")
-    .then(data => setItems(data.data));
+    .then(data => data.data);
+    let MRP = data.reduce((acc,curr)=>acc+curr.old_price,0);
+    console.log("MRP",MRP);
+    setMRP(MRP);
+    let off = data.reduce((acc,curr)=>acc+(curr.old_price/100)*curr.disscount,0);
+    console.log("off",off);
+    setOff(off);
+    setItems(data);
   }
-  console.log("cartData",items);
+  console.log("items",items);
+  
+
+  const deleteCartItem = async(id)=>{
+     await axios.delete(`http://localhost:8080/cart/${id}`);
+     getCartData();
+  }
+
 
   useEffect(()=>{
     getCartData();
@@ -19,6 +35,7 @@ export const Cart = () => {
 
   return (
     <Box mt="50px" bgColor="whitesmoke" px={10}>
+      {items.length ? (
         <Box p={2} display="flex" flexDirection={{base:"column", md:"row"}} gap={5}>
           <Box flex={{base:"12",md:"8"}}  p={2}>
             <Text  my={5} align="center" fontWeight="semibold">YOUR ITEM's</Text>
@@ -33,15 +50,17 @@ export const Cart = () => {
                         <Text fontWeight="semibold">{item.title}</Text>
                         <Text>{item.unit}</Text>
                         <Box display="flex" gap={5} pt={4} alignItems="center">
-                          <Text fontWeight="semibold" fontSize="2xl">₹{item.price}</Text>
+                          {/* <Text fontWeight="semibold" fontSize="2xl">₹{item.price}</Text> */}
+                          <Text fontWeight="semibold" fontSize="2xl">₹{item.old_price-(item.old_price)/100*item.disscount}</Text>
                           <Text as="del" >₹{item.old_price}</Text>
-                          <Text color="green">{item.disscount} off</Text>
+                          {/* <Text color="green">{item.disscount} off</Text> */}
+                          <Text color="green">₹{(item.old_price)/100*item.disscount} off</Text>
                         </Box>
                       </Box>
                     </Box>
                     <Box flex={3} >
                       <Box border="1px solid black" borderRadius={5} align="center" mt={4}>
-                        <Button variant="unstyled" rightIcon={<DeleteIcon></DeleteIcon>}>Remove</Button>
+                        <Button onClick={()=>deleteCartItem(item.id)} variant="unstyled" rightIcon={<DeleteIcon></DeleteIcon>}>Remove</Button>
                       </Box>
                     </Box>
                   </Box>
@@ -54,19 +73,28 @@ export const Cart = () => {
             <Box border="1px solid black">
               <Box display="flex" p={2} gap={2} justifyContent="space-between">
                 <Text >MRP ({items.length} Items) </Text>
-                <Text fontWeight="semibold">₹{}</Text>
+                <Text fontWeight="semibold">₹{MRP}</Text>
               </Box>
               <Box display="flex" b p={2} gap={2} justifyContent="space-between">
                 <Text >Product Discount</Text>
-                <Text fontWeight="semibold" color="green">₹{}</Text>
+                <Text fontWeight="semibold" color="green">₹{off}</Text>
               </Box>
               <Box display="flex" p={2} gap={2} justifyContent="space-between">
                 <Text >Delivery Fee</Text>
                 <Text fontWeight="semibold" color="red">₹ 49</Text>
               </Box>
+              <Divider my={2} variant="dashed" colorScheme="blackAlpha"></Divider>
+              <Box display="flex" p={2} gap={2} justifyContent="space-between">
+                <Text >Total</Text>
+                <Text fontWeight="semibold" color="green">₹ {MRP-off+49}</Text>
+              </Box>
             </Box>
           </Box>
-        </Box>
+        </Box> ) : (
+          <Box bgColor="white" m="auto">
+            <Text>Your cart is Empty</Text>
+          </Box>
+        )}
     </Box>
   )
 }
